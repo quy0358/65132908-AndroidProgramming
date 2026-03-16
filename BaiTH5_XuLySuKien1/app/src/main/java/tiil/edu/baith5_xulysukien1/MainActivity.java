3954ba78adab9d4965e450aa51d1f4d9829bb26c
quy0358 <153436485+quy0358@users.noreply.github.com>
2026-03-16 19:07:02 +0700

    hoàn thành BaiTH5_XuLySuKien1

--- BaiTH5_XuLySuKien1/app/src/main/java/tiil/edu/baith5_xulysukien1/MainActivity.java
+++ BaiTH5_XuLySuKien1/app/src/main/java/tiil/edu/baith5_xulysukien1/MainActivity.java
@@ -1,6 +1,9 @@
 package tiil.edu.baith5_xulysukien1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextSo1, editTextSo2, editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,85 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTextSo1 = findViewById(R.id.editTextSo1);
+        editTextSo2 = findViewById(R.id.editTextSo2);
+        editTextKQ = findViewById(R.id.editTextKQ);
+        nutCong = findViewById(R.id.nutCong);
+        nutTru = findViewById(R.id.nutTru);
+        nutNhan = findViewById(R.id.nutNhan);
+        nutChia = findViewById(R.id.nutChia);
+
+        nutCong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XULY_CONG();
+            }
+        });
+
+        nutTru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XULY_TRU();
+            }
+        });
+
+        nutNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XULY_NHAN();
+            }
+        });
+
+        nutChia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                XULY_CHIA();
+            }
+        });
+    }
+
+    void XULY_CONG() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tong = num1 + num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+
+    void XULY_TRU() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tong = num1 - num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+
+    void XULY_NHAN() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        float tong = num1 * num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
+    }
+
+    void XULY_CHIA() {
+        String so1 = editTextSo1.getText().toString();
+        String so2 = editTextSo2.getText().toString();
+        float num1 = Float.parseFloat(so1);
+        float num2 = Float.parseFloat(so2);
+        if (num2 == 0) {
+            editTextKQ.setText("Lỗi: không thể chia cho 0");
+            return;
+        }
+        float tong = num1 / num2;
+        String chuoiKQ = String.valueOf(tong);
+        editTextKQ.setText(chuoiKQ);
     }
 }
